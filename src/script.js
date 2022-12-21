36784a77900a6640d538b6e7103195ec3c5823a5
Michal1898 <michal.volf@gmail.com>
2022-12-21 18:41:12 +0100

    step = 1

--- src/script.js
+++ src/script.js
@@ -3,17 +3,17 @@ state = 50
 onkeydown = (event) => {
 
   if(event.key == "a") {
-    state=state - 5
+    state=state - 1  
   }
-  if (event.key == "l") { state=state + 5
+  if (event.key == "l") { state=state + 1
 }
   
-if (state <= 0) {
+if (state <= 30) {
   alert('vyhral hrac 😁')
   state = 50
 }
   
- if (state => 100) {
+ if (state => 70) {
   alert('vyhral hrac 👍')
   state = 50
 }
